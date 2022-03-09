9921d51be6ebb9505383947927cffb08e7505c42
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-03-09 12:24:28 +0800

    done a star but slow

--- searching/informed.py
+++ searching/informed.py
@@ -1,22 +1,33 @@
-from collections import Counter
-from turtle import distance #to act like a std::map on cpp
-
-#reference: https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2 08/03/2022
+from collections import Counter #to act like a std::map on cpp
+import os
+#pseudocode reference: 
+#https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2 08/03/2022
 class PuzzleNode():
     """A node class for 8 Puzzle"""
-    def __init__(self, parent=None, position=None, prev_move=None):
-        self.parent = parent #a string of 9 char
-        self.position = position #also a string of 9 char
-        self.prev_move = prev_move
+    def __init__(self, parent=None, state=None, prev_move=None):
+        self.parent = parent #a node :D
+        self.state = state #also a string of 9 char
+        self.prev_move = prev_move #[r, l, d, u] move to get from parrent to this node
         self.g = 0 #cost from start node
         self.h = 0 #estimated cost to end node
         self.f = 0 #total cost
+        self.zero_id = None #location of "0" in the string to fasten look up for moveset
+        #search for 0
+        # print(type(self.state))
+        for id, c in enumerate(self.state):
+            c = int(c)
+            if(c == 0):
+                self.zero_id = id
+                # print(f'zero id: {id}')
+                break
+
+
 
     def __eq__(self, other):
-        return self.position == other.position
+        return self.state == other.state
 
     def __str__(self):
-        return self.position
+        return self.state
 
 #GLOBAL VARIABLE
 GOAL_NODE = None #will be created after readfile
@@ -39,7 +50,7 @@ def readfile(filename):
     # print(data) #is a string
     return data
 
-def get_pos_from_str(string_state):
+def get_pos_from_state(string_state):
     positions = [
         [],
         [],
@@ -59,11 +70,10 @@ def get_pos_from_str(string_state):
     
     return positions
 
-
-def get_heuristic_val(game_position):
+def get_heuristic_val(node_state):
     global GOAL_POS
     total_distance = 0
-    for id, c in enumerate(game_position):
+    for id, c in enumerate(node_state):
         c = int(c)
         if(c == 0):#skip 0 position
             continue
@@ -72,8 +82,106 @@ def get_heuristic_val(game_position):
         total_distance += distance
     return total_distance
 
-def a_star(game_state, zero_id):
-    pass
+def get_min_node(open_list):
+    min_node = open_list[0] #set current min node as the first element
+    node_id = 0 #id to be removed from open list
+    for id, node in enumerate(open_list):
+        if(node.f < min_node.f):
+            #update the node and id
+            min_node = node
+            node_id = id
+    
+    return min_node, node_id
+
+def create_state(cur_node, move):
+    state_list = list(cur_node.state) #because string is immutable
+    num_id = 0 #swapped number index
+    if(move == 'r'):
+        #zero to left
+        num_id = cur_node.zero_id - 1
+    elif (move == 'l'):
+        #zero to right
+        num_id = cur_node.zero_id + 1
+    elif (move == 'u'):
+        #zero to down
+        num_id = cur_node.zero_id + 2
+    else:
+        #zero to up
+        num_id = cur_node.zero_id - 2
+    
+    #swap
+    state_list[cur_node.zero_id] = state_list[num_id] #set 0 to number
+    state_list[num_id] = "0" #set the num to 0
+
+    state_str = "".join(state_list)
+    # print(state_list)
+    #return the state
+    return state_str
+
+def reconstruct_path(node):
+    path = []
+    cur_node = node
+    while(cur_node is not None):
+        path.append(cur_node.prev_move)
+        cur_node = cur_node.parent
+    
+    return path[::-1] #return the reversed the path
+
+def a_star(start_node):
+    global GOAL_NODE, MOVE_SET
+    open_nodes = [] #store node that haven't explored
+    closed_state = Counter() #store state that has been explored
+    path = None #saved path for return value
+    open_nodes.append(start_node) #put start node on the open list
+    
+    #loop while open list is not empty in pythonic way
+    while open_nodes:
+        #get node with min f value
+        min_node, node_id = get_min_node(open_nodes)
+        #remove the min node from open list
+        open_nodes.pop(node_id)
+        #add min node counter in 
+        closed_state[min_node.state]+=1
+        
+        #check if it is the goal node
+        if (min_node == GOAL_NODE):
+            print("HEY, Found the goal!")
+            path = reconstruct_path(min_node)
+            break
+        
+        #get all possible move
+        possible_move = MOVE_SET[min_node.zero_id]
+        # print(f'got {len(possible_move)} possible move')
+        for move in possible_move:
+            #generate node based on move
+            move_state = create_state(min_node, move)
+            # print(move_state)
+            move_node = PuzzleNode(min_node, move_state, move)
+            # os.system("pause")
+            #check if this node's state has been reached/visited/closed
+            if(closed_state[move_node.state] > 0):
+                continue
+            
+            print(f'{move_node.state}')
+            #calculate f value of this node
+            move_node.g = min_node.g + 1
+            move_node.h = get_heuristic_val(move_node.state)
+            move_node.f = move_node.g + move_node.h
+
+            worthyToAdd = True
+            #check this node duplicate on open_nodes using linear search
+            for node in open_nodes:
+                if(move_node == node):
+                    if(move_node.g > node.g):
+                        #same node exist but with lower g cost, so threw this node
+                        worthyToAdd = False
+                        break
+            
+            if(worthyToAdd):
+                open_nodes.append(move_node)
+        #End of For Loop
+    #End of While Loop
+    return path
 
 def greedy_best_first(game_state):
     pass
@@ -88,21 +196,18 @@ def main():
     init_state = readfile("state.txt")
     goal_state = readfile("goal.txt")
     
+    print(init_state)
+    print(goal_state)
     #create nodes based on those state
     start_node = PuzzleNode(None, init_state)
     GOAL_NODE = PuzzleNode(None, goal_state)
     
-    #extract each number row/col position for heuristic
-    GOAL_POS = get_pos_from_str(GOAL_NODE.position)
+    #extract each number row/col position for heuristic calculation
+    GOAL_POS = get_pos_from_state(GOAL_NODE.state)
 
-    h = get_heuristic_val(start_node.position)
-    print(h)
-
-
-
-
-
-
+    #search!
+    path = a_star(start_node)
+    print(path)
 
 
 
