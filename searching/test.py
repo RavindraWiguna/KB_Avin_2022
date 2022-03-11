0903d1acde1ad1cbc3e42c54dc54df5f174f0edd
RavindraWiguna <ravindrawiguna@gmail.com>
2022-03-11 21:06:04 +0800

    some experiments

--- searching/test.py
+++ searching/test.py
@@ -1,20 +1,20 @@
 import time
+TENFIVE = 10**5
 
 def ordf():
     ground = ord("0")
     char = "2"
     i = 0
     res = 0
-    while( i < 100000):
+    while( i < TENFIVE):
         res = ord(char) - ground
         i+=1
-
 
 def intf():
     char = "2"
     i = 0
     res = 0
-    while( i < 100000):
+    while( i < TENFIVE):
         res = int(char)
         i+=1
 
@@ -22,17 +22,15 @@ def intcompsame():
     integers = 12345678
     new_int = 12345678
     i = 0
-    while( i < 100000):
+    while( i < TENFIVE):
         check = integers == new_int
         i+=1
-
-
 
 def intcompdif():
     integers = 201345678
     new_int = 501234678
     i = 0
-    while(i < 100000):
+    while(i < TENFIVE):
         check = integers == new_int
         i+=1
 
@@ -40,7 +38,7 @@ def strcompsame():
     strings = "12345678"
     new_str = "12345678"
     i = 0
-    while(i < 100000):
+    while(i < TENFIVE):
         check = strings == new_str
         i+=1
 
@@ -48,18 +46,41 @@ def strcompdif():
     strings = "201345678"
     new_str = "501234678"
     i = 0
-    while(i < 100000):
+    while(i < TENFIVE):
         check = strings == new_str
         i+=1
 
-start_time = time.perf_counter()
+def plusid():
+    temp_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    tseven = 0
+    i = 0
+    while(i < TENFIVE):
+        i+=1
+        for id, el in enumerate(temp_arr):
+            if(el == 7):
+                tseven += id
+                break
+    
 
+def overwriteid():
+    temp_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    tseven = 0
+    i = 0
+    while(i < TENFIVE):
+        i+=1
+        for id, el in enumerate(temp_arr):
+            if(el == 7):
+                tseven = id
+                break    
+
+start_time = time.perf_counter()
+plusid()
 end_time = time.perf_counter()
 
 print(f'elapsed time: {end_time - start_time}')
 
 start_time = time.perf_counter()
-strcompdif()
+overwriteid()
 end_time = time.perf_counter()
 
 print(f'elapsed time: {end_time - start_time}')
